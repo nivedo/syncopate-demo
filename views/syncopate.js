07187a3405217fefb90d3ac0579f9c96a1499afa
Jay Ni <jayxni@gmail.com>
2015-08-31 16:02:30 -0500

    bugfix

--- views/syncopate.js
+++ views/syncopate.js
@@ -2,18 +2,21 @@ var liveData = {}
 
 document.addEventListener('DOMContentLoaded', function () {
     regex=/\{\{\s*([^}\s-]+)\s*\}\}/;
+    wsurl="ws://api.blub.io:32804/ws?";
     var elems = document.body.getElementsByTagName("*");
     for (var i = elems.length; i--;) {
         content=elems[i].innerHTML;
         match=content.match(regex);
         if(match) {
-            elems[i].id = match[1];
+            elems[i].id = match[1].replace(/\./g,":");
+            wsurl=wsurl+"series="+match[1]+"&";
             elems[i].textContent="";
         }
     }
 
     if (window["WebSocket"]) {
-        conn = new WebSocket("ws://api.blub.io:32804/ws?series=testcluster.num_threads&series=testcluster.cpu_usage&series=testcluster.aapl_price");
+        console.log(wsurl);
+        conn = new WebSocket(wsurl);
         conn.onmessage = function(evt) {
             console.log(evt.data)
             parseData(evt.data);
