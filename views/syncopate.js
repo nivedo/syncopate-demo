cf7dc7cf657d1c0c2bd5c57267ab44958036265c
Jay Ni <jayxni@gmail.com>
2015-08-31 20:40:08 -0500

    updating syncopate js

--- views/syncopate.js
+++ views/syncopate.js
@@ -1,46 +1,50 @@
-var liveData = {}
+var Syncopate = Syncopate || (function(){
+    var liveData = {}
 
-document.addEventListener('DOMContentLoaded', function () {
-    regex=/\{\{\s*([^}\s-]+)\s*\}\}/;
-    wsurl="ws://api.blub.io:32804/ws?";
-    var elems = document.body.getElementsByTagName("*");
-    for (var i = elems.length; i--;) {
-        content=elems[i].innerHTML;
-        match=content.match(regex);
-        if(match) {
-            elems[i].id = match[1].replace(/\./g,":");
-            wsurl=wsurl+"series="+match[1]+"&";
-            elems[i].textContent="";
+    return {
+        init : function(token) {
+            regex=/\{\{\s*([^}\s-]+)\s*\}\}/;
+            wsurl="ws://api.blub.io:32815/ws?token=" + token;
+            var elems = document.body.getElementsByTagName("*");
+            for (var i = elems.length; i--;) {
+                content=elems[i].innerHTML;
+                match=content.match(regex);
+                if(match) {
+                    elems[i].className = elems[i].className + " " + match[1];
+                    wsurl=wsurl+"&series="+match[1];
+                    elems[i].textContent="";
+                }
+            }
+
+            if (window["WebSocket"]) {
+                conn = new WebSocket(wsurl);
+                conn.onmessage = function(evt) {
+                    Syncopate.parse(evt.data);
+                }
+            }
+        },
+        parse : function(rawData) {
+            data = JSON.parse(rawData);
+            for(i = 0; i < data["Series"].length; i++) {
+                var id = data["Series"][i]["Key"]
+                var response = data["Series"][i]["Response"]
+                for (var key in response["Snapshot"]) {
+                    liveData[id] = response["Snapshot"][key];
+                }
+            }
+        },
+        update : function(data) {
+            for (var key in data) {
+                var elems = document.getElementsByClassName(key);
+                for (var i = elems.length; i--;) {
+                    elems[i].textContent=data[key];
+                }
+            }
+        },
+        run : function() {
+            setInterval(function() {
+                Syncopate.update(liveData);
+            }, 100);
         }
-    }
-
-    if (window["WebSocket"]) {
-        console.log(wsurl);
-        conn = new WebSocket(wsurl);
-        conn.onmessage = function(evt) {
-            console.log(evt.data)
-            parseData(evt.data);
-        }
-    }
-});
-
-function parseData(rawData){
-    console.log(rawData);
-    data = JSON.parse(rawData);
-    for(i = 0; i < data["Series"].length; i++) {
-        for (var key in data["Series"][i]["Snapshot"]) {
-            liveData["testcluster:" + key] = data["Series"][i]["Snapshot"][key];
-        }
-    }
-}
-
-function update(data) {
-    for (var key in data) {
-        document.getElementById(key).textContent=data[key];
-    }
-}
-
-// Add a random value to each line every second
-setInterval(function() {
-    update(liveData);
-}, 100);
+    };
+}());
